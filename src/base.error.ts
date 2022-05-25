b746f239d522a33d44b3cd9436fea63f052a0dd1
Danial Manavi <dmanavi@live.com>
2022-05-24 23:18:18 -0700

    Change constructor properties order.

--- src/base.error.ts
+++ src/base.error.ts
@@ -13,7 +13,7 @@ export abstract class BaseError<TCode = ErrorCodeType> extends Error {
    */
   readonly message: string;
 
-  constructor(message: string, code?: TCode, error?: Error) {
+  constructor(message: string, error?: Error, code?: TCode) {
     /* Validation phase */
     if (isNonEmptyString(message) === false) {
       throw new Error(`The message property should be a non-empty string.`);
@@ -23,8 +23,8 @@ export abstract class BaseError<TCode = ErrorCodeType> extends Error {
     super(message);
 
     this.message = message;
-    this.code = isNullOrUndefined(code) === false ? code : BaseError.name as any;
     this.stack = error?.stack;
+    this.code = isNullOrUndefined(code) === false ? code : BaseError.name as any;
 
     // set stacktrace
     Error.captureStackTrace(this, BaseError);
