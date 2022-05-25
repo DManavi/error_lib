d78c061145f4f4566dade896211fa55b7aa66953
Danial Manavi <dmanavi@live.com>
2022-05-24 23:40:29 -0700

    Set file indentation to 2 spaces.

--- src/http.error.ts
+++ src/http.error.ts
@@ -5,51 +5,51 @@ import { ErrorOptions } from './types';
 
 
 export type HttpErrorOptions = ErrorOptions & {
-    /**
-     * Status code
-     */
-    statusCode: number;
+  /**
+   * Status code
+   */
+  statusCode: number;
 
-    /**
-     * Status message
-     */
-    statusMessage?: string;
+  /**
+   * Status message
+   */
+  statusMessage?: string;
 
-    /**
-     * Is this a handled error
-     */
-    isHandled?: boolean;
+  /**
+   * Is this a handled error
+   */
+  isHandled?: boolean;
 };
 
 export class HttpError extends BaseError {
 
-    /**
-     * HTTP status regarding the error
-     */
-    public readonly statusCode: number;
+  /**
+   * HTTP status regarding the error
+   */
+  public readonly statusCode: number;
 
-    /**
-     * HTTP status message regarding the error
-     */
-    public readonly statusMessage?: string;
+  /**
+   * HTTP status message regarding the error
+   */
+  public readonly statusMessage?: string;
 
-    /**
-     * Is this a handled error
-     */
-    public readonly isHandled?: boolean;
+  /**
+   * Is this a handled error
+   */
+  public readonly isHandled?: boolean;
 
-    constructor(message?: string, error?: Error, opts?: HttpErrorOptions) {
-        /* Initialization phase */
-        super(message, error, opts);
+  constructor(message?: string, error?: Error, opts?: HttpErrorOptions) {
+    /* Initialization phase */
+    super(message, error, opts);
 
-        this.statusCode = opts?.statusCode || statusCodes.INTERNAL_SERVER_ERROR;
-        this.statusMessage = opts?.statusMessage || getReasonPhrase(this.statusCode);
-        this.isHandled = opts?.isHandled || false;
+    this.statusCode = opts?.statusCode || statusCodes.INTERNAL_SERVER_ERROR;
+    this.statusMessage = opts?.statusMessage || getReasonPhrase(this.statusCode);
+    this.isHandled = opts?.isHandled || false;
 
-        // set stacktrace
-        Error.captureStackTrace(this, HttpError);
+    // set stacktrace
+    Error.captureStackTrace(this, HttpError);
 
-        // Set prototype to make instanceOf enabled
-        Object.setPrototypeOf(this, HttpError.prototype);
-    }
+    // Set prototype to make instanceOf enabled
+    Object.setPrototypeOf(this, HttpError.prototype);
+  }
 }
