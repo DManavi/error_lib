8ef6997d8df1a0f3721d5bde3f82cade0515cc1a
Danial Manavi <dmanavi@live.com>
2022-05-24 23:31:31 -0700

    ApplicationError class.

---
+++ src/application.error.ts
@@ -0,0 +1,17 @@
+import { BaseError } from './base.error';
+import { ErrorOptions } from './types';
+
+/**
+ * Application Error
+ */
+export class ApplicationError extends BaseError {
+  constructor(message: string, error?: Error, opts?: ErrorOptions) {
+    super(message, error, opts);
+
+    // set stacktrace
+    Error.captureStackTrace(this, ApplicationError);
+
+    // Set prototype to make instanceOf enabled
+    Object.setPrototypeOf(this, ApplicationError.prototype);
+  }
+}
