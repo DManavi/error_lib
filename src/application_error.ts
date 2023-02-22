5f873669cb208d0cb1c0fa6a15d6c704ef36403f
Danial Manavi <dmanavi@live.com>
2023-02-21 16:02:30 -0800

    feat[application_error]: The base error for all the other error types.

--- src/application_error.ts
+++ src/application_error.ts
@@ -1,27 +1,71 @@
-import { BaseError } from './base_error';
-import { ErrorOptions } from './types';
+import { Class } from 'type-fest';
 
-const CLASS_NAME = 'ApplicationError';
-
-/**
- * Application Error
- */
-export class ApplicationError extends BaseError {
+export interface ApplicationErrorConstructorOptions<
+  TCauseError extends Error = Error,
+> {
   /**
-   * @constructor
-   * @param {string | undefined} message Error message
-   * @param {ErrorOptions | undefined} opts Error options
-   * @returns {ApplicationError} New instance of the application error
+   * Error code
    */
-  constructor(message?: string, opts?: ErrorOptions) {
-    super(message ?? CLASS_NAME, {
-      ...opts,
-      code: opts?.code ?? CLASS_NAME,
-    });
+  code?: string;
 
-    // set stacktrace
-    Error.captureStackTrace(this, ApplicationError);
-    // Set prototype to make instanceOf enabled
-    Object.setPrototypeOf(this, ApplicationError.prototype);
+  /**
+   * The cause of this error (or the parent error)
+   */
+  cause?: TCauseError;
+}
+
+export class ApplicationError<TCause extends Error = Error> extends Error {
+  /**
+   * Error code (e.g. E_APPLICATION_ERROR)
+   */
+  public readonly code: string;
+
+  /**
+   * The cause of this error (or the parent error)
+   */
+  public readonly cause?: Error;
+
+  /**
+   *
+   * @param message Custom error message
+   * @param opts Extra options
+   */
+  constructor(
+    message?: string,
+    opts?: ApplicationErrorConstructorOptions<TCause>,
+  ) {
+    message = message ?? 'ApplicationError';
+
+    super(message);
+
+    this.code = opts?.code ?? 'E_APPLICATION_ERROR';
+    this.cause = opts?.cause;
+
+    this.configureSubError(ApplicationError);
+  }
+
+  /**
+   * Configure sub error
+   * @param cls Constructor/class of the error
+   */
+  protected configureSubError<T>(cls: Class<T>) {
+    this.captureStackTrace(cls);
+    this.setPrototypeOf(cls);
+  }
+
+  /**
+   * Capture stack trace in the error
+   * @param cls Constructor/class of the error
+   */
+  private captureStackTrace<T>(cls: Class<T>) {
+    Error.captureStackTrace(this, cls);
+  }
+
+  /**
+   * Set prototype of the object to enable instanceOf operator
+   * @param cls Constructor/class of the error
+   */
+  private setPrototypeOf<T>(cls: Class<T>) {
+    Object.setPrototypeOf(this, cls.prototype);
   }
 }
