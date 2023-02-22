0272a4480a794b08d88350075a1faa26294ded8b
Danial Manavi <dmanavi@live.com>
2023-02-21 16:13:31 -0800

    feat[error_lib]: Export newly added errors.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-// Base
-export * from './base_error';
-export * from './types';
-// Application
 export * from './application_error';
 export * from './bad_request_error';
 export * from './forbidden_error';
 export * from './not_found_error';
-// utilities
-export * as utils from './utils';
+export * from './resource_not_found_error';
+export * from './route_not_found_error';
+export * from './validation_error';
