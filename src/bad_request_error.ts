9199c96a381ce2b3189ea628d48e1d39711c6da7
Danial Manavi <dmanavi@live.com>
2023-02-21 16:08:35 -0800

    feat[bad_request]: BadRequestError class.

--- src/bad_request_error.ts
+++ src/bad_request_error.ts
@@ -1,45 +1,28 @@
-import { ApplicationError } from './application_error';
-import { ErrorOptions } from './types';
-import { safeGetProperty } from './utils';
+import {
+  ApplicationError,
+  ApplicationErrorConstructorOptions,
+} from './application_error';
 
-const CLASS_NAME = 'BadRequest';
+export interface BadRequestErrorConstructorOptions<
+  TCauseError extends Error = Error,
+> extends ApplicationErrorConstructorOptions<TCauseError> {}
 
-export type BadRequestErrorOptions<TClientErrors = void> = ErrorOptions &
-  (TClientErrors extends void
-    ? {}
-    : {
-        /**
-         * Client errors
-         */
-        clientErrors: TClientErrors;
-      });
-
-/**
- * BadRequestError
- */
-export class BadRequestError<TClientErrors = void> extends ApplicationError {
+export class BadRequestError<
+  TCause extends Error = Error,
+> extends ApplicationError<TCause> {
   /**
-   * Client error(s)
+   *
+   * @param message Custom error message
+   * @param opts Extra options
    */
-  public readonly clientErrors?: TClientErrors;
+  constructor(
+    message?: string,
+    opts?: BadRequestErrorConstructorOptions<TCause>,
+  ) {
+    message = message ?? 'BadRequest';
 
-  /**
-   * @constructor
-   * @param {string | undefined} message Error message
-   * @param {BadRequestErrorOptions | undefined} opts Error options
-   */
-  constructor(message?: string, opts?: BadRequestErrorOptions<TClientErrors>) {
-    super(message ?? CLASS_NAME, {
-      error: opts?.error,
-      code: opts?.code ?? CLASS_NAME,
-    });
+    super(message, { cause: opts?.cause, code: 'E_BAD_REQUEST' });
 
-    /* Class-specific parameters */
-    this.clientErrors = safeGetProperty<any>(opts, 'clientErrors');
-
-    // set stacktrace
-    Error.captureStackTrace(this, BadRequestError);
-    // Set prototype to make instanceOf enabled
-    Object.setPrototypeOf(this, BadRequestError.prototype);
+    this.configureSubError(BadRequestError);
   }
 }
